d15765fa39c7af20b9166f9b2e12b93afa747556
ACER <sumit.jaiswal8888@gmail.com>
2024-09-17 23:43:35 +0530

    first commit

---
+++ src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,17 @@
+package com.example.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableFeignClients
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
